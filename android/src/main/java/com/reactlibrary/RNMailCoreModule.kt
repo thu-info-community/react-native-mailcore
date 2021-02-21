b4755942aeba4c825dbd84ae0e18755db7ab16f8
UNIDY2002 <UNIDY2002@outlook.com>
2021-02-21 11:19:58 +0800

    Convert `RNMailCoreModule` to Kotlin

--- android/src/main/java/com/reactlibrary/RNMailCoreModule.kt
+++ android/src/main/java/com/reactlibrary/RNMailCoreModule.kt
@@ -1,213 +1,114 @@
+package com.reactlibrary
 
-package com.reactlibrary;
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.bridge.ReactContextBaseJavaModule
+import com.reactlibrary.MailClient
+import com.facebook.react.bridge.ReactMethod
+import com.facebook.react.bridge.ReadableMap
+import com.facebook.react.bridge.Promise
 
-import com.facebook.react.bridge.Promise;
-import com.facebook.react.bridge.ReactApplicationContext;
-import com.facebook.react.bridge.ReactContextBaseJavaModule;
-import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.bridge.ReadableMap;
+class RNMailCoreModule(reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext) {
+    private val mailClient = MailClient()
 
-public class RNMailCoreModule extends ReactContextBaseJavaModule {
+    override fun getName() = "RNMailCore"
 
-  private final ReactApplicationContext reactContext;
-  private final MailClient mailClient;
-
-  public RNMailCoreModule(ReactApplicationContext reactContext) {
-    super(reactContext);
-    this.reactContext = reactContext;
-    this.mailClient = new MailClient();
-  }
-
-  @Override
-  public String getName() {
-    return "RNMailCore";
-  }
-
-  @ReactMethod
-  public void loginImap(final ReadableMap obj, final Promise promise){
-    getCurrentActivity().runOnUiThread(new Runnable() {
-      @Override
-      public void run() {
-        UserCredential user = new UserCredential(obj);
-        mailClient.initIMAPSession(user,promise);
-      }
-    });
-  }
-
-  @ReactMethod
-  public void loginSmtp(final ReadableMap obj, final Promise promise){
-    getCurrentActivity().runOnUiThread(new Runnable() {
-      @Override
-      public void run() {
-        UserCredential user = new UserCredential(obj);
-        mailClient.initSMTPSession(user,promise);
-      }
-    });
-  }
-
-  @ReactMethod
-  public void createFolder(final ReadableMap obj,final Promise promise) {
-    getCurrentActivity().runOnUiThread(new Runnable() {
-      @Override
-      public void run() {
-        mailClient.createFolderLabel(obj,promise);
-      }
-    });
-  }
-
-  @ReactMethod
-  public void renameFolder(final ReadableMap obj,final Promise promise) {
-    getCurrentActivity().runOnUiThread(new Runnable() {
-      @Override
-      public void run() {
-        mailClient.renameFolderLabel(obj,promise);
-      }
-    });
-  }
-
-  @ReactMethod
-  public void deleteFolder(final ReadableMap obj,final Promise promise) {
-    getCurrentActivity().runOnUiThread(new Runnable() {
-      @Override
-      public void run() {
-        mailClient.deleteFolderLabel(obj,promise);
-      }
-    });
-  }
-  
-  @ReactMethod
-  public void getFolders(final Promise promise) {
-    getCurrentActivity().runOnUiThread(new Runnable() {
-      @Override
-      public void run() {
-        mailClient.getFolders(promise);
-      }
-    });
-  }
-  
-  @ReactMethod
-  public void moveEmail(final ReadableMap obj, final Promise promise) {
-    getCurrentActivity().runOnUiThread(new Runnable() {
-      @Override
-      public void run() {
-        mailClient.moveEmail(obj,promise);
-      }
-    });
-  }
-
-  @ReactMethod
-  public void permantDeleteEmail(final ReadableMap obj, final Promise promise) {
-    getCurrentActivity().runOnUiThread(new Runnable() {
-      @Override
-      public void run() {
-        mailClient.permantDelete(obj, promise);
-      }
-    });
-  }
-
-  @ReactMethod
-  public void actionFlagMessage(final ReadableMap obj, final Promise promise) {
-    getCurrentActivity().runOnUiThread(new Runnable() {
-      @Override
-      public void run() {
-        mailClient.ActionFlagMessage(obj, promise);
-      }
-    });
-  }
-
-  @ReactMethod
-  public void actionLabelMessage(final ReadableMap obj, final Promise promise) {
-    getCurrentActivity().runOnUiThread(new Runnable() {
-      @Override
-      public void run() {
-        mailClient.ActionLabelMessage(obj, promise);
-      }
-    });
-  }
-
-  @ReactMethod
-  public void sendMail(final ReadableMap obj, final Promise promise) {
-      mailClient.sendMail(obj, promise, getCurrentActivity());
-  }
-
-  @ReactMethod
-  public void getMail(final ReadableMap obj, final Promise promise) {
-    getCurrentActivity().runOnUiThread(new Runnable() {
-      @Override
-      public void run() {
-        mailClient.getMail(obj, promise);
-      }
-    });
-  }
-
-@ReactMethod
-  public void getAttachment(final ReadableMap obj, final Promise promise) {
-    getCurrentActivity().runOnUiThread(new Runnable() {
-      @Override
-      public void run() {
-        mailClient.getAttachment(obj, promise);
-      }
-    });
-  }
-
-   @ReactMethod
-    public void getAttachmentInline(final ReadableMap obj, final Promise promise) {
-        getCurrentActivity().runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                mailClient.getAttachmentInline(obj, promise);
-            }
-        });
+    @ReactMethod
+    fun loginImap(obj: ReadableMap, promise: Promise) {
+        currentActivity!!.runOnUiThread {
+            mailClient.initIMAPSession(UserCredential(obj), promise)
+        }
     }
 
-  @ReactMethod
-  public void getMails(final ReadableMap obj, final Promise promise) {
-    getCurrentActivity().runOnUiThread(new Runnable() {
-      @Override
-      public void run() {
-        mailClient.getMails(obj, promise);
-      }
-    });
-  }
+    @ReactMethod
+    fun loginSmtp(obj: ReadableMap, promise: Promise) {
+        currentActivity!!.runOnUiThread {
+            mailClient.initSMTPSession(UserCredential(obj), promise)
+        }
+    }
 
-  @ReactMethod
-  public void getMailsThread(final ReadableMap obj, final Promise promise) {
-    getCurrentActivity().runOnUiThread(new Runnable() {
-      @Override
-      public void run() {
-        mailClient.getMailsThread(obj, promise);
-      }
-    });
-  }
+    @ReactMethod
+    fun createFolder(obj: ReadableMap, promise: Promise) {
+        currentActivity!!.runOnUiThread { mailClient.createFolderLabel(obj, promise) }
+    }
 
-  @ReactMethod
-  public void statusFolder(final ReadableMap obj, final Promise promise) {
-    getCurrentActivity().runOnUiThread(new Runnable() {
-      @Override
-      public void run() {
-        mailClient.statusFolder(obj,promise);
-      }
-    });
-  }
+    @ReactMethod
+    fun renameFolder(obj: ReadableMap, promise: Promise) {
+        currentActivity!!.runOnUiThread { mailClient.renameFolderLabel(obj, promise) }
+    }
 
-  @ReactMethod
-  public void getMailsByRange(final ReadableMap obj, final Promise promise) {
-    getCurrentActivity().runOnUiThread(new Runnable() {
-      @Override
-      public void run() {
-        mailClient.getMailsByRange(obj,promise);
-      }
-    });
-  }
+    @ReactMethod
+    fun deleteFolder(obj: ReadableMap, promise: Promise) {
+        currentActivity!!.runOnUiThread { mailClient.deleteFolderLabel(obj, promise) }
+    }
 
-  @ReactMethod
-  public void getMailsByThread(final ReadableMap obj, final Promise promise) {
-    getCurrentActivity().runOnUiThread(new Runnable() {
-      @Override
-      public void run() {
-        mailClient.getMailsByThread(obj,promise);
-      }
-    });
-  }
+    @ReactMethod
+    fun getFolders(promise: Promise?) {
+        currentActivity!!.runOnUiThread { mailClient.getFolders(promise) }
+    }
+
+    @ReactMethod
+    fun moveEmail(obj: ReadableMap, promise: Promise) {
+        currentActivity!!.runOnUiThread { mailClient.moveEmail(obj, promise) }
+    }
+
+    @ReactMethod
+    fun permantDeleteEmail(obj: ReadableMap, promise: Promise) {
+        currentActivity!!.runOnUiThread { mailClient.permantDelete(obj, promise) }
+    }
+
+    @ReactMethod
+    fun actionFlagMessage(obj: ReadableMap, promise: Promise) {
+        currentActivity!!.runOnUiThread { mailClient.ActionFlagMessage(obj, promise) }
+    }
+
+    @ReactMethod
+    fun actionLabelMessage(obj: ReadableMap, promise: Promise) {
+        currentActivity!!.runOnUiThread { mailClient.ActionLabelMessage(obj, promise) }
+    }
+
+    @ReactMethod
+    fun sendMail(obj: ReadableMap, promise: Promise) {
+        currentActivity!!.runOnUiThread { mailClient.sendMail(obj, promise, currentActivity) }
+    }
+
+    @ReactMethod
+    fun getMail(obj: ReadableMap, promise: Promise) {
+        currentActivity!!.runOnUiThread { mailClient.getMail(obj, promise) }
+    }
+
+    @ReactMethod
+    fun getAttachment(obj: ReadableMap, promise: Promise) {
+        currentActivity!!.runOnUiThread { mailClient.getAttachment(obj, promise) }
+    }
+
+    @ReactMethod
+    fun getAttachmentInline(obj: ReadableMap, promise: Promise) {
+        currentActivity!!.runOnUiThread { mailClient.getAttachmentInline(obj, promise) }
+    }
+
+    @ReactMethod
+    fun getMails(obj: ReadableMap, promise: Promise) {
+        currentActivity!!.runOnUiThread { mailClient.getMails(obj, promise) }
+    }
+
+    @ReactMethod
+    fun getMailsThread(obj: ReadableMap, promise: Promise) {
+        currentActivity!!.runOnUiThread { mailClient.getMailsThread(obj, promise) }
+    }
+
+    @ReactMethod
+    fun statusFolder(obj: ReadableMap, promise: Promise) {
+        currentActivity!!.runOnUiThread { mailClient.statusFolder(obj, promise) }
+    }
+
+    @ReactMethod
+    fun getMailsByRange(obj: ReadableMap, promise: Promise) {
+        currentActivity!!.runOnUiThread { mailClient.getMailsByRange(obj, promise) }
+    }
+
+    @ReactMethod
+    fun getMailsByThread(obj: ReadableMap, promise: Promise) {
+        currentActivity!!.runOnUiThread { mailClient.getMailsByThread(obj, promise) }
+    }
 
 }
