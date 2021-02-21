8a2ba352da751ee11fb75ac51904889dcab0e058
UNIDY2002 <UNIDY2002@outlook.com>
2021-02-21 10:53:52 +0800

    Convert `UserCredential` to Kotlin

---
+++ android/src/main/java/com/reactlibrary/UserCredential.kt
@@ -0,0 +1,11 @@
+package com.reactlibrary
+
+import com.facebook.react.bridge.ReadableMap
+
+class UserCredential(obj: ReadableMap) {
+    val username: String? = obj.getString("username")
+    val password: String? = obj.getString("password")
+    val hostname: String? = obj.getString("hostname")
+    val port: Int = obj.getInt("port")
+    val authType: Int = obj.getInt("authType")
+}
