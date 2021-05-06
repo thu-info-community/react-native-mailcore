4961016cb2b294bffe34bb7ced47e32f1a5000a9
UNIDY2002 <UNIDY2002@outlook.com>
2021-05-06 14:19:25 +0800

    Add type definition

---
+++ index.d.ts
@@ -0,0 +1,82 @@
+interface LoginConfig {
+    hostname: string;
+    port: number;
+    username: string;
+    password: string;
+    authType: number;
+}
+
+interface Folder {
+    path: string;
+    flags: number;
+}
+
+interface Attachment {
+    filename: string;
+    size: string;
+    encoding: number;
+    uniqueId: string;
+}
+
+interface InlineAttachment extends Attachment {
+    cid: string;
+    partID: string;
+    mimepart: string;
+}
+
+interface Mail {
+    headers: {[key: string]: string};
+    id: number;
+    flags: number;
+    from: string;
+    subject: string;
+    date: string;
+    attachments: string;
+}
+
+interface MailDetail extends Omit<Omit<Mail, "attachments">, "from"> {
+    from: {mailbox: string; displayName: string};
+    to: {[key: string]: string};
+    cc?: {[key: string]: string};
+    bcc?: {[key: string]: string};
+    body: string;
+    attachments: {[key: number]: Attachment};
+    inline: {[key: number]: InlineAttachment};
+}
+
+type Response<T> = Promise<T & {status: string}>;
+
+export function loginImap(config: LoginConfig): Response<{}>;
+
+export function loginSmtp(config: LoginConfig): Response<{}>;
+
+export function getFolders(): Response<{folders: Folder[]}>;
+
+export function getMails({
+    folder,
+    requestKind,
+}: {
+    folder: string;
+    requestKind: number;
+}): Response<{mails: Mail[]}>;
+
+export function getMail({
+    folder,
+    messageId,
+    requestKind,
+}: {
+    folder: string;
+    messageId: number;
+    requestKind: number;
+}): Response<MailDetail>;
+
+export function sendMail({}: {
+    headers?: {[key: string]: string};
+    from: {addressWithDisplayName: string; mailbox: string};
+    to: {[mailbox: string]: string};
+    cc?: {[mailbox: string]: string};
+    bcc?: {[mailbox: string]: string};
+    subject?: string;
+    body: string;
+    attachments?: string[];
+}): Response<{}>;
